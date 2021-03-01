ac71a8f81add081421812e1fb481fd4d867e9edf
jid <jid@hwtc.com.cn>
2021-03-01 17:18:28 +0800

    [add]:添加单例模式示例和观察者模式示例

--- SingleInstance.py
+++ SingleInstance.py
@@ -4,24 +4,26 @@ user:Created by jid on 2021/2/26
 email:jid@hwtc.com.cn
 description:单例模式
 """
+import threading
 
 
-class S:
-    instance = None
+class Singleton(object):
+    _instance_lock = threading.Lock()
 
-    def __new__(cls, *args, **kwargs):
-        if S.instance is None:
-            S.instance = super().__new__(cls)
+    @classmethod
+    def instance(cls):
+        if not hasattr(Singleton, "_instance"):
+            with Singleton._instance_lock:
+                if not hasattr(Singleton, "_instance"):
+                    Singleton._instance = Singleton()
+        return Singleton._instance
 
-    def __init__(self):
-        pass
 
-
-s1 = S()
+s1 = Singleton().instance()
 print(id(s1))
-s2 = S()
+s2 = Singleton().instance()
 print(id(s2))
-s3 = S()
+s3 = Singleton().instance()
 print(id(s3))
-s4 = S()
+s4 = Singleton().instance()
 print(id(s4))
